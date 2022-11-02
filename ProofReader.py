d8bcb10ed58f9e758b7b19b684ad3244847f6e00
The Nightman <102193772+The-Nightman@users.noreply.github.com>
2022-11-02 14:55:13 +0000

    Update ProofReader.py
    added final spell check functionality, no longer updating as application beyond this point. may integrate into text writing apps at later date with higher functionality

--- ProofReader.py
+++ ProofReader.py
@@ -1,12 +1,17 @@
+import string
+
+word_dict = open('words.txt')
+
 proofread_input = input("Enter file path here:\n")
 
 proofread_file = open(proofread_input)
 
-fileextension = proofread_input.split(".")
+file_extension = proofread_input.split(".")
 
-while fileextension[-1] != "txt":
-    proofread_file = input(("unsupported file type, please enter path of txt file:"))
-    fileextension = proofread_file.split(".")
+while file_extension[-1] != "txt":
+    proofread_file = input(
+        ("unsupported file type, please enter path of txt file:"))
+    file_extension = proofread_file.split(".")
 
 file_read = proofread_file.read()
 
@@ -25,7 +30,8 @@ for i in file_read:
     elif i.isdigit():
         num += 1
 
-specialchar_count = underscore_count + character_count - (letter + num + space_count + (line_count - 1))
+specialchar_count = underscore_count + character_count - \
+    (letter + num + space_count + (line_count - 1))
 
 print(f"Characters (with spaces): {character_count}")
 print(f"Characters (no spaces): {character_count - space_count}")
@@ -36,3 +42,17 @@ print(f"Special characters: {specialchar_count}")
 print(f"Lines: {line_count}")
 print(f"Words: {word_count}")
 print(f"Paragraphs: {paragraph_count}")
+
+spelling_reference = word_dict.read().casefold().translate(
+    str.maketrans('', '', string.punctuation)).split()
+
+user_spelling_input = file_read.casefold().translate(
+    str.maketrans('', '', string.punctuation)).split()
+
+for item in user_spelling_input[:]:
+    if item in spelling_reference:
+        user_spelling_input.remove(item)
+    else:
+        pass
+
+print(f"please check for possible spelling mistakes:\n{user_spelling_input}")
